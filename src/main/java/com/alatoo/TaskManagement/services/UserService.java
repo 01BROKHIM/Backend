c3b8b2ae115a2c7fc657453fc7d4275e8ff1e006
01BROKHIM <ibroxim1337p@gmail.com>
2024-03-20 23:27:36 +0600

    Implemented services for handling business logic

---
+++ src/main/java/com/alatoo/TaskManagement/services/UserService.java
@@ -0,0 +1,38 @@
+package com.alatoo.TaskManagement.services;
+
+import com.alatoo.TaskManagement.entities.User;
+import com.alatoo.TaskManagement.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return (List<User>) userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+
+}
+
