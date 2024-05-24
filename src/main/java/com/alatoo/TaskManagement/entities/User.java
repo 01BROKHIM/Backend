554b31e35f9311eec7f5fdc6cea0d4d8056643fd
01BROKHIM <ibrokhim.pazylov@alatoo.edu.kg>
2024-05-24 22:00:49 +0600

    Added password to user and userrole

--- src/main/java/com/alatoo/TaskManagement/entities/User.java
+++ src/main/java/com/alatoo/TaskManagement/entities/User.java
@@ -1,9 +1,16 @@
 package com.alatoo.TaskManagement.entities;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.*;
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotBlank;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -15,10 +22,27 @@ import java.util.List;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue
     private Long id;
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 4, max = 50, message = "Username must be between 4 and 50 characters")
+    @Column(unique = true)
+    @Pattern(regexp = "^[a-z0-9_]+$")
     private String username;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
     private String email;
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @ManyToMany(cascade = CascadeType.PERSIST, fetch = FetchType.EAGER)
+    private Set<UserRole> roles;
 
     @OneToMany(mappedBy = "user")
     private List<Task> tasks;
